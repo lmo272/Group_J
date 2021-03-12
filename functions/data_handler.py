ba5f8e155b94cd9996fe4900f66474ae0f70585d
Niclas <niclas.sturm@live.de>
2021-03-12 10:54:49 +0100

    Linted data_handler.py. Updated README with appropriate information.

--- functions/data_handler.py
+++ functions/data_handler.py
@@ -15,13 +15,12 @@ Functions:
 import math
 import os
 import zipfile
+import datetime
 from urllib.request import urlretrieve
 from dataenforce import Dataset
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import datetime
-
 
 
 class DataHandler:
@@ -48,7 +47,8 @@ class DataHandler:
         Plots the average total rentals by month of the year
     forecast(self, month: int):
         Plots the bike rental forecast for a 168-hour period (a Mon-Sun week)
-        corresponding to the average rental with a shaded area corresponding to an interval of [-1 std deviation, +1 std deviation.].
+        corresponding to the average rental with a shaded area corresponding to an
+        interval of [-1 std deviation, +1 std deviation.].
     """
 
     def __init__(self, dataframe: Dataset = pd.DataFrame()):
@@ -73,7 +73,8 @@ class DataHandler:
         Nothing
         Example
         ---------
-        download_file("https://archive.ics.uci.edu/ml/machine-learning-databases/00275/Bike-Sharing-Dataset.zip",
+        download_file(
+        "https://archive.ics.uci.edu/ml/machine-learning-databases/00275/Bike-Sharing-Dataset.zip",
         output_file='Bike-Sharing-Dataset.zip')
         """
 
@@ -107,8 +108,8 @@ class DataHandler:
         self.dataframe = pd.read_csv(zip_contents.open(csv_file))
         self.dataframe.dteday = pd.to_datetime(self.dataframe.dteday)
         self.dataframe = self.dataframe.set_index("dteday")
-        self.dataframe.index +=  pd.to_timedelta( self.dataframe.hr, unit='h')
-        
+        self.dataframe.index += pd.to_timedelta(self.dataframe.hr, unit='h')
+
     # Method 3
     def plot_correlation_matrix(self):
         """
@@ -119,12 +120,13 @@ class DataHandler:
         Seaborn correlation matrix
         """
         sns.heatmap(
-            self.dataframe[["mnth", "weathersit", "temp", "windspeed", "cnt"]].corr(), annot=True
-        )
-        plt.title("Correlation between month, weathersituation, temperature, windspeed and bike rentals", 
-                  fontdict={"weight" : "bold",
-                            "size" : 20})
-    
+            self.dataframe[["mnth", "weathersit", "temp", "windspeed", "cnt"]].corr(),
+            annot=True)
+        plt.title(
+            "Correlation between month, weathersituation, temperature, windspeed and bike rentals",
+            fontdict={"weight": "bold",
+                      "size": 20})
+
     # Method 4
     def plot_weekly_data(self, week: int):
         """
@@ -155,21 +157,19 @@ class DataHandler:
         self.dataframe["week"] = self.dataframe.index.map(date_week)
 
         plt.style.use("seaborn")
-        self.dataframe.loc[self.dataframe["week"] == week].\
+        self.dataframe.loc[self.dataframe["week"] == week]. \
             plot("instant", "cnt", figsize=(15, 9))
         plt.title(f"Number of Bike rentals in week {week}",
                   fontsize=20, fontweight="bold")
         plt.ylabel("# of bike rentals", fontsize=16)
         plt.xlabel("instant", fontsize=16)
         plt.show()
-           
-    #Method 5
+
+    # Method 5
     def plot_monthly_average(self):
         """
         Plots the average total rentals by month of the year
-        Parameters
-        ------------
-        None
+
         Returns
         ------------
         Vertical Barchart with the average total rentals by month of the year
@@ -178,55 +178,64 @@ class DataHandler:
         plot_monthly_average()
         """
         plt.style.use("seaborn")
-        plt.bar(x=range(1,13), 
-                height=self.dataframe.groupby(self.dataframe.index.month).sum("cnt")["cnt"].values*0.5)
+        plt.bar(x=range(1, 13),
+                height=self.dataframe
+                .groupby(self.dataframe.index.month).sum("cnt")["cnt"].values * 0.5)
         plt.title("Average total rentals by month of the year",
-                   fontsize=20, fontweight="bold")
+                  fontsize=20, fontweight="bold")
         plt.ylabel("# of bike rentals", fontsize=16)
         plt.xlabel("month", fontsize=16)
         plt.show()
-        
-    #Method 6
+
+    # Method 6
     def forecast(self, month: int):
         """
         Plots the bike rental forecast for a 168-hour period (a Mon-Sun week) as a blue line
         and the rental forecast [-1 std deviation, +1 std deviation.], shown in the shaded area.
-                
+
         Parameters
         ------------
         month: int
             A integer between 1-12 inclusive, representing the months of a year (January-December)
-        df_month
-        
+
         Returns
         ---------
-        A plot with two visual elements. The plot has a 168h period on the x-axis and the number of bikes rented
-        on the y-axis
-        The first visual element is the blue line, showing the expected rentals for that week based on the average rentals of the two years in the dataset.
-        The second visual elements is the gray shayed area, representing the expected rental for the week
-        +and- one standart deviation.
+        A plot with two visual elements. The plot has a 168h period on the x-axis
+        and the number of bikes rented on the y-axis.
+
+        The first visual element is the blue line,
+        showing the expected rentals for that week based on the
+        average rentals of the two years in the dataset.
+
+        The second visual elements is the gray shayed area,
+        representing the expected rental for the week
+        +and - one standard deviation.
         Example
         ---------
         forecast(3)
         """
+        if month > 12 or month < 0:
+            raise ValueError("""Specified month outside of allowed range.
+                             Month must be an integer between 1 and 12.""")
+
         df_month = self.dataframe.loc[self.dataframe.index.month == month]
-        df_month = df_month.groupby(["weekday","hr"]).cnt.agg(["mean", "std"])
+        df_month = df_month.groupby(["weekday", "hr"]).cnt.agg(["mean", "std"])
         df_month.index = df_month.index.get_level_values(1)
         df_month = df_month.reset_index()
-        fig, ax = plt.subplots(figsize=(15,9))
+        fig, axis = plt.subplots(figsize=(15, 9))
 
         datetime_object = datetime.datetime.strptime(str(month), "%m")
         full_month_name = datetime_object.strftime("%B")
 
-        ax.fill_between(df_month.index,
-                        df_month['mean']-df_month['std'],
-                        df_month['mean']+df_month['std'],
-                        alpha=0.4)
+        axis.fill_between(df_month.index,
+                          df_month['mean'] - df_month['std'],
+                          df_month['mean'] + df_month['std'],
+                          alpha=0.4)
 
-        df_month['mean'].plot(ax=ax)
+        df_month['mean'].plot(ax=axis)
 
-        ax.set_ylabel("# of bike rentals", fontsize=16)
-        ax.set_xlabel("hour", fontsize=16)
+        axis.set_ylabel("# of bike rentals", fontsize=16)
+        axis.set_xlabel("hour", fontsize=16)
 
         plt.title(f"Expected weekly rentals in {full_month_name}",
                   fontsize=20, fontweight="bold")
